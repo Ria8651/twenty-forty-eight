5c537d3e6f03b74c77444e83e4c6e5bf6eaf57b0
Ria8651 <bk.dev@icloud.com>
2022-09-24 13:42:41 +1200

    add update board event

--- src/board.rs
+++ src/board.rs
@@ -4,12 +4,52 @@ pub struct BoardPlugin;
 
 impl Plugin for BoardPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(setup);
+        app.add_event::<UpdateBoardEvent>()
+            .insert_resource(Board {
+                data: vec![
+                    vec![0, 1, 1, 2],
+                    vec![1, 0, 2, 1],
+                    vec![1, 1, 2, 1],
+                    vec![2, 3, 4, 5],
+                ],
+            })
+            .add_startup_system(setup)
+            .add_system(update_board);
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Board {
+    data: Vec<Vec<u8>>,
+}
+
+struct UpdateBoardEvent(Board);
+
 #[derive(Component)]
-struct Tile;
+struct Tile(u32);
+#[derive(Component)]
+struct TileText(u32);
+
+fn update_board(
+    mut board: ResMut<Board>,
+    mut events: EventReader<UpdateBoardEvent>,
+    // mut tile_query: Query<&mut Tile>,
+    mut tile_text_query: Query<(&TileText, &mut Text)>,
+) {
+    for event in events.iter() {
+        *board = event.0.clone();
+    }
+
+    for (tile_text, mut text) in tile_text_query.iter_mut() {
+        let exp = board.data[3 - tile_text.0 as usize / 4][tile_text.0 as usize % 4];
+        if exp == 0 {
+            text.sections[0].value = "".to_string();
+        } else {
+            let value = 1u32 << exp as u32;
+            text.sections[0].value = value.to_string();
+        }
+    }
+}
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands
@@ -40,7 +80,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                     ..default()
                 })
                 .with_children(|parent| {
-                    for _ in 0..16 {
+                    for i in 0..16 {
                         parent
                             .spawn_bundle(NodeBundle {
                                 style: Style {
@@ -57,17 +97,18 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                                 color: Color::rgb(0.6, 0.6, 0.6).into(),
                                 ..default()
                             })
+                            .insert(Tile(i))
                             .with_children(|parent| {
-                                parent.spawn_bundle(
-                                    TextBundle::from_section(
+                                parent
+                                    .spawn_bundle(TextBundle::from_section(
                                         "2",
                                         TextStyle {
                                             font: asset_server.load("fonts/FiraSans-Bold.ttf"),
                                             font_size: 60.0,
                                             color: Color::WHITE,
                                         },
-                                    )
-                                );
+                                    ))
+                                    .insert(TileText(i));
                             });
                     }
                 });
