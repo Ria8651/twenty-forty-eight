0b883d9850b2f3deec3ad9f64ec8c12dc4515476
Ria8651 <bk.dev@icloud.com>
2023-10-27 22:45:53 +1300

    minimax search

--- src/board.rs
+++ src/board.rs
@@ -1,80 +1,119 @@
-use bevy::prelude::*;
 use rand::Rng;
 
-pub struct BoardPlugin;
-
-impl Plugin for BoardPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_event::<UpdateBoardEvent>()
-            // .insert_resource(Board {
-            //     data: vec![
-            //         vec![3, 2, 1, 1],
-            //         vec![4, 5, 6, 7],
-            //         vec![11, 10, 9, 8],
-            //         vec![12, 13, 14, 15],
-            //     ],
-            // })
-            .insert_resource(Board::new())
-            .add_systems(Startup, setup)
-            .add_systems(Update, update_board);
-    }
-}
-
-#[derive(Resource, Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Board {
     pub data: Vec<Vec<u8>>,
+    pub player_to_move: bool,
 }
 
 impl Board {
     pub fn new() -> Self {
-        let mut board = Board {
+        Board {
             data: vec![vec![0; 4]; 4],
-        };
-
-        board.add_random();
-        board.add_random();
-
-        board
+            player_to_move: true,
+        }
     }
 
-    pub fn swipe(&mut self, direction: Direction) {
-        let mut new_board = self.clone();
+    /// Returns true if the board was changed
+    pub fn swipe(&mut self, direction: Swipe) -> bool {
+        let mut changed = false;
         for a in 0..4 {
             let mut last = (a, 0);
             for b in 1..4 {
-                let pos = abtoxy(a, b, direction);
-                let current = new_board.data[pos.y][pos.x];
+                let pos = direction.abtoxy(a, b);
+                let current = self.data[pos.y][pos.x];
 
-                let last_pos = abtoxy(last.0, last.1, direction);
-                let last_value = new_board.data[last_pos.y][last_pos.x];
+                let last_pos = direction.abtoxy(last.0, last.1);
+                let last_value = self.data[last_pos.y][last_pos.x];
 
                 if current != 0 {
                     if current == last_value {
-                        new_board.data[pos.y][pos.x] = 0;
-                        new_board.data[last_pos.y][last_pos.x] += 1;
+                        self.data[pos.y][pos.x] = 0;
+                        self.data[last_pos.y][last_pos.x] += 1;
                         last = (last.0, last.1 + 1);
+
+                        changed = true;
                     } else if last_value == 0 {
-                        new_board.data[pos.y][pos.x] = 0;
-                        new_board.data[last_pos.y][last_pos.x] = current;
+                        self.data[pos.y][pos.x] = 0;
+                        self.data[last_pos.y][last_pos.x] = current;
+
+                        changed = true;
                     } else {
                         last = (last.0, last.1 + 1);
-                        let last_pos = abtoxy(last.0, last.1, direction);
+                        let last_pos = direction.abtoxy(last.0, last.1);
                         if last_pos != pos {
-                            new_board.data[pos.y][pos.x] = 0;
-                            new_board.data[last_pos.y][last_pos.x] = current;
+                            self.data[pos.y][pos.x] = 0;
+                            self.data[last_pos.y][last_pos.x] = current;
+
+                            changed = true;
                         }
                     }
                 }
             }
         }
+        changed
+    }
 
-        if new_board != *self {
-            self.data = new_board.data;
-            self.add_random();
+    pub fn get_moves(&self) -> Vec<Moves> {
+        match self.player_to_move {
+            true => {
+                let mut moves = Vec::new();
+                for swipe in [Swipe::Up, Swipe::Down, Swipe::Left, Swipe::Right].iter() {
+                    let mut board = self.clone();
+                    if board.swipe(*swipe) {
+                        moves.push(Moves::Player(*swipe));
+                    }
+                }
+                moves
+            }
+            false => {
+                let mut moves = Vec::new();
+                for y in 0..4 {
+                    for x in 0..4 {
+                        if self.data[y][x] == 0 {
+                            moves.push(Moves::Computer(ComputerMoves::PlaceTwo(Pos::new(x, y))));
+                        }
+                    }
+                }
+                moves
+            }
         }
     }
 
-    fn add_random(&mut self) {
+    pub fn apply_move(&mut self, moves: Moves) {
+        match moves {
+            Moves::Player(swipe) => {
+                self.swipe(swipe);
+                self.player_to_move = false;
+            }
+            Moves::Computer(computer_moves) => match computer_moves {
+                ComputerMoves::PlaceTwo(pos) => {
+                    self.data[pos.y][pos.x] = 1;
+                    self.player_to_move = true;
+                }
+                ComputerMoves::PlaceFour(pos) => {
+                    self.data[pos.y][pos.x] = 2;
+                    self.player_to_move = true;
+                }
+            },
+        }
+    }
+
+    pub fn score(&self) -> u32 {
+        let mut score = 0;
+        for y in 0..4 {
+            for x in 0..4 {
+                let exp = self.data[y][x] as u32;
+                if exp > 0 {
+                    let x = 1 << exp;
+                    score += exp * x - x;
+                }
+            }
+        }
+        score
+    }
+
+    pub fn add_random(&mut self) {
         let mut empty_tiles = Vec::new();
         for y in 0..4 {
             for x in 0..4 {
@@ -96,23 +135,9 @@ impl Board {
             };
         }
     }
-
-    pub fn score(&self) -> u32 {
-        let mut score = 0;
-        for y in 0..4 {
-            for x in 0..4 {
-                let exp = self.data[y][x] as u32;
-                if exp > 0 {
-                    let x = 1 << exp;
-                    score += exp * x - x;
-                }
-            }
-        }
-        score
-    }
 }
 
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Pos {
     pub x: usize,
     pub y: usize,
@@ -124,214 +149,52 @@ impl Pos {
     }
 }
 
-const fn abtoxy(a: usize, b: usize, direction: Direction) -> Pos {
-    match direction {
-        Direction::Up => Pos::new(a, b),
-        Direction::Down => Pos::new(a, 3 - b),
-        Direction::Left => Pos::new(b, a),
-        Direction::Right => Pos::new(3 - b, a),
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-pub enum Direction {
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Swipe {
     Up,
     Down,
     Left,
     Right,
 }
 
-#[derive(Event)]
-pub struct UpdateBoardEvent;
-
-#[derive(Component)]
-struct Tile(u32);
-#[derive(Component)]
-struct TileText(u32);
-
-#[derive(Component)]
-struct ScoreText;
-#[derive(Component)]
-struct TimeText;
-
-const fn color_map(exp: u8) -> Color {
-    match exp {
-        0 => Color::rgb(0.80, 0.76, 0.71),
-        1 => Color::rgb(0.93, 0.90, 0.85),
-        2 => Color::rgb(0.93, 0.89, 0.79),
-        3 => Color::rgb(0.95, 0.7, 0.48),
-        4 => Color::rgb(0.96, 0.59, 0.39),
-        5 => Color::rgb(0.97, 0.49, 0.37),
-        6 => Color::rgb(0.97, 0.37, 0.24),
-        7..=11 => Color::rgb(0.93, 0.82, 0.45),
-        _ => Color::rgb(0.4, 0.4, 0.4),
+impl Swipe {
+    fn abtoxy(&self, a: usize, b: usize) -> Pos {
+        match self {
+            Swipe::Up => Pos::new(a, b),
+            Swipe::Down => Pos::new(a, 3 - b),
+            Swipe::Left => Pos::new(b, a),
+            Swipe::Right => Pos::new(3 - b, a),
+        }
     }
 }
 
-fn update_board(
-    board: Res<Board>,
-    mut update_event: EventReader<UpdateBoardEvent>,
-    mut querys: ParamSet<(
-        Query<(&Tile, &mut BackgroundColor)>,
-        Query<(&TileText, &mut Text)>,
-        Query<&mut Text, With<ScoreText>>,
-        Query<&mut Text, With<TimeText>>,
-    )>,
-    time: Res<Time>,
-) {
-    for _ in update_event.iter() {
-        for (tile, mut ui_colour) in querys.p0().iter_mut() {
-            let exp = board.data[3 - tile.0 as usize / 4][tile.0 as usize % 4];
-            *ui_colour = color_map(exp).into();
-        }
-
-        for (tile_text, mut text) in querys.p1().iter_mut() {
-            let exp = board.data[3 - tile_text.0 as usize / 4][tile_text.0 as usize % 4];
-
-            let string = if exp == 0 {
-                "".to_string()
-            } else {
-                (1u32 << exp as u32).to_string()
-            };
-            text.sections[0].value = string;
-            text.sections[0].style.color = if exp <= 3 {
-                Color::rgb(0.47, 0.44, 0.40)
-            } else {
-                Color::rgb(0.98, 0.96, 0.95)
-            };
-        }
-
-        let mut score_query = querys.p2();
-        let mut score_text = score_query.single_mut();
-        score_text.sections[0].value = board.score().to_string();
-
-        let mut time_query = querys.p3();
-        let mut time_text = time_query.single_mut();
-        time_text.sections[0].value = format!("{:.2}", time.startup().elapsed().as_secs_f32());
-    }
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ComputerMoves {
+    PlaceTwo(Pos),
+    PlaceFour(Pos),
 }
 
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut update_event: EventWriter<UpdateBoardEvent>,
-) {
-    commands
-        .spawn(NodeBundle {
-            style: Style {
-                width: Val::Percent(100.0),
-                height: Val::Percent(100.0),
-                justify_content: JustifyContent::Center,
-                align_items: AlignItems::Center,
-                ..default()
-            },
-            background_color: Color::rgb(0.98, 0.97, 0.94).into(),
-            ..default()
-        })
-        .with_children(|parent| {
-            parent
-                .spawn(NodeBundle {
-                    style: Style {
-                        width: Val::Px(400.0),
-                        height: Val::Percent(100.0),
-                        flex_direction: FlexDirection::ColumnReverse,
-                        align_items: AlignItems::Center,
-                        justify_content: JustifyContent::Center,
-                        ..default()
-                    },
-                    background_color: Color::NONE.into(),
-                    ..default()
-                })
-                .with_children(|parent| {
-                    parent
-                        .spawn(NodeBundle {
-                            style: Style {
-                                width: Val::Percent(100.0),
-                                height: Val::Px(80.0),
-                                flex_direction: FlexDirection::Row,
-                                align_items: AlignItems::Center,
-                                justify_content: JustifyContent::SpaceBetween,
-                                ..default()
-                            },
-                            background_color: Color::NONE.into(),
-                            ..default()
-                        })
-                        .with_children(|parent| {
-                            parent
-                                .spawn(TextBundle::from_section(
-                                    "0",
-                                    TextStyle {
-                                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                                        font_size: 60.0,
-                                        color: Color::rgb(0.47, 0.44, 0.40),
-                                    },
-                                ))
-                                .insert(ScoreText);
-                            parent
-                                .spawn(TextBundle::from_section(
-                                    "0",
-                                    TextStyle {
-                                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                                        font_size: 60.0,
-                                        color: Color::rgb(0.47, 0.44, 0.40),
-                                    },
-                                ))
-                                .insert(TimeText);
-                        });
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Moves {
+    Computer(ComputerMoves),
+    Player(Swipe),
+}
 
-                    parent
-                        .spawn(NodeBundle {
-                            style: Style {
-                                width: Val::Px(400.0),
-                                height: Val::Px(400.0),
-                                padding: UiRect {
-                                    top: Val::Px(10.0),
-                                    left: Val::Px(10.0),
-                                    ..default()
-                                },
-                                flex_wrap: FlexWrap::Wrap,
-                                ..default()
-                            },
-                            background_color: Color::rgb(0.73, 0.68, 0.63).into(),
-                            ..default()
-                        })
-                        .with_children(|parent| {
-                            for i in 0..16 {
-                                parent
-                                    .spawn(NodeBundle {
-                                        style: Style {
-                                            width: Val::Px(87.5),
-                                            height: Val::Px(87.5),
-                                            margin: UiRect {
-                                                bottom: Val::Px(10.0),
-                                                right: Val::Px(10.0),
-                                                ..default()
-                                            },
-                                            justify_content: JustifyContent::Center,
-                                            align_items: AlignItems::Center,
-                                            ..default()
-                                        },
-                                        background_color: Color::rgb(0.6, 0.6, 0.6).into(),
-                                        ..default()
-                                    })
-                                    .insert(Tile(i))
-                                    .with_children(|parent| {
-                                        parent
-                                            .spawn(TextBundle::from_section(
-                                                i.to_string(),
-                                                TextStyle {
-                                                    font: asset_server
-                                                        .load("fonts/FiraSans-Bold.ttf"),
-                                                    font_size: 40.0,
-                                                    color: Color::rgb(0.47, 0.44, 0.40),
-                                                },
-                                            ))
-                                            .insert(TileText(i));
-                                    });
-                            }
-                        });
-                });
-        });
-
-    update_event.send(UpdateBoardEvent);
+impl std::fmt::Display for Board {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut board = String::new();
+        for y in 0..4 {
+            for x in 0..4 {
+                let tile = self.data[y][x] as u32;
+                if tile == 0 {
+                    board.push_str("    ");
+                } else {
+                    let tile = 1 << tile;
+                    board.push_str(&format!("{:4}", tile));
+                }
+            }
+            board.push('\n');
+        }
+        write!(f, "{}", board)
+    }
 }
---
+++ src/render.rs
@@ -0,0 +1,207 @@
+use bevy::prelude::*;
+
+pub struct BoardPlugin;
+
+impl Plugin for BoardPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_event::<UpdateBoardEvent>()
+            .insert_resource(Board::new())
+            .add_systems(Startup, setup)
+            .add_systems(Update, update_board);
+    }
+}
+
+#[derive(Event)]
+pub struct UpdateBoardEvent;
+
+#[derive(Component)]
+struct Tile(u32);
+#[derive(Component)]
+struct TileText(u32);
+
+#[derive(Component)]
+struct ScoreText;
+#[derive(Component)]
+struct TimeText;
+
+const fn color_map(exp: u8) -> Color {
+    match exp {
+        0 => Color::rgb(0.80, 0.76, 0.71),
+        1 => Color::rgb(0.93, 0.90, 0.85),
+        2 => Color::rgb(0.93, 0.89, 0.79),
+        3 => Color::rgb(0.95, 0.7, 0.48),
+        4 => Color::rgb(0.96, 0.59, 0.39),
+        5 => Color::rgb(0.97, 0.49, 0.37),
+        6 => Color::rgb(0.97, 0.37, 0.24),
+        7..=11 => Color::rgb(0.93, 0.82, 0.45),
+        _ => Color::rgb(0.4, 0.4, 0.4),
+    }
+}
+
+fn update_board(
+    board: Res<Board>,
+    mut update_event: EventReader<UpdateBoardEvent>,
+    mut querys: ParamSet<(
+        Query<(&Tile, &mut BackgroundColor)>,
+        Query<(&TileText, &mut Text)>,
+        Query<&mut Text, With<ScoreText>>,
+        Query<&mut Text, With<TimeText>>,
+    )>,
+    time: Res<Time>,
+) {
+    for _ in update_event.iter() {
+        for (tile, mut ui_colour) in querys.p0().iter_mut() {
+            let exp = board.data[3 - tile.0 as usize / 4][tile.0 as usize % 4];
+            *ui_colour = color_map(exp).into();
+        }
+
+        for (tile_text, mut text) in querys.p1().iter_mut() {
+            let exp = board.data[3 - tile_text.0 as usize / 4][tile_text.0 as usize % 4];
+
+            let string = if exp == 0 {
+                "".to_string()
+            } else {
+                (1u32 << exp as u32).to_string()
+            };
+            text.sections[0].value = string;
+            text.sections[0].style.color = if exp <= 3 {
+                Color::rgb(0.47, 0.44, 0.40)
+            } else {
+                Color::rgb(0.98, 0.96, 0.95)
+            };
+        }
+
+        let mut score_query = querys.p2();
+        let mut score_text = score_query.single_mut();
+        score_text.sections[0].value = board.score().to_string();
+
+        let mut time_query = querys.p3();
+        let mut time_text = time_query.single_mut();
+        time_text.sections[0].value = format!("{:.2}", time.startup().elapsed().as_secs_f32());
+    }
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut update_event: EventWriter<UpdateBoardEvent>,
+) {
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            background_color: Color::rgb(0.98, 0.97, 0.94).into(),
+            ..default()
+        })
+        .with_children(|parent| {
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        width: Val::Px(400.0),
+                        height: Val::Percent(100.0),
+                        flex_direction: FlexDirection::ColumnReverse,
+                        align_items: AlignItems::Center,
+                        justify_content: JustifyContent::Center,
+                        ..default()
+                    },
+                    background_color: Color::NONE.into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent
+                        .spawn(NodeBundle {
+                            style: Style {
+                                width: Val::Percent(100.0),
+                                height: Val::Px(80.0),
+                                flex_direction: FlexDirection::Row,
+                                align_items: AlignItems::Center,
+                                justify_content: JustifyContent::SpaceBetween,
+                                ..default()
+                            },
+                            background_color: Color::NONE.into(),
+                            ..default()
+                        })
+                        .with_children(|parent| {
+                            parent
+                                .spawn(TextBundle::from_section(
+                                    "0",
+                                    TextStyle {
+                                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                        font_size: 60.0,
+                                        color: Color::rgb(0.47, 0.44, 0.40),
+                                    },
+                                ))
+                                .insert(ScoreText);
+                            parent
+                                .spawn(TextBundle::from_section(
+                                    "0",
+                                    TextStyle {
+                                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                        font_size: 60.0,
+                                        color: Color::rgb(0.47, 0.44, 0.40),
+                                    },
+                                ))
+                                .insert(TimeText);
+                        });
+
+                    parent
+                        .spawn(NodeBundle {
+                            style: Style {
+                                width: Val::Px(400.0),
+                                height: Val::Px(400.0),
+                                padding: UiRect {
+                                    top: Val::Px(10.0),
+                                    left: Val::Px(10.0),
+                                    ..default()
+                                },
+                                flex_wrap: FlexWrap::Wrap,
+                                ..default()
+                            },
+                            background_color: Color::rgb(0.73, 0.68, 0.63).into(),
+                            ..default()
+                        })
+                        .with_children(|parent| {
+                            for i in 0..16 {
+                                parent
+                                    .spawn(NodeBundle {
+                                        style: Style {
+                                            width: Val::Px(87.5),
+                                            height: Val::Px(87.5),
+                                            margin: UiRect {
+                                                bottom: Val::Px(10.0),
+                                                right: Val::Px(10.0),
+                                                ..default()
+                                            },
+                                            justify_content: JustifyContent::Center,
+                                            align_items: AlignItems::Center,
+                                            ..default()
+                                        },
+                                        background_color: Color::rgb(0.6, 0.6, 0.6).into(),
+                                        ..default()
+                                    })
+                                    .insert(Tile(i))
+                                    .with_children(|parent| {
+                                        parent
+                                            .spawn(TextBundle::from_section(
+                                                i.to_string(),
+                                                TextStyle {
+                                                    font: asset_server
+                                                        .load("fonts/FiraSans-Bold.ttf"),
+                                                    font_size: 40.0,
+                                                    color: Color::rgb(0.47, 0.44, 0.40),
+                                                },
+                                            ))
+                                            .insert(TileText(i));
+                                    });
+                            }
+                        });
+                });
+        });
+
+    update_event.send(UpdateBoardEvent);
+}
